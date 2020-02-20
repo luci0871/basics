c2f8c90ae02a6eb04435cac9fd84887e170b894f
Luciano de Souza <luci0871@stud.kea.dk>
2020-02-20 10:42:09 +0100

    first late commit with some index and javascript code

---
+++ basics.js
@@ -0,0 +1,28 @@
+"use strict"
+/* const name = "LucciGucci";
+const cat = "Mandu";
+const
+    console.log("My name is $("
+        name ").
+
+        I have a pet name $("cat "); */
+
+/* const name = "Albus Percival Wulfric Brian Dumbledore";
+console.log(name);
+const len = name.length;
+const letter = name[0];
+console.log(letter);
+console.log(`${name} is ${len} characters long`); */
+
+/* const fullName = "Luciano de Souza";
+const firstName = fullName.substring(0.5);
+console.log(fullName) */
+
+/* const name = "Albus Percival Wulfric Brian Dumbledore";
+const fullName = name.substring(0.5);
+console.log(name); */
+
+/* const santa = "🎅";
+santa.codePointAt(0);
+console.log(santa.codePointAt(0));
+ */
